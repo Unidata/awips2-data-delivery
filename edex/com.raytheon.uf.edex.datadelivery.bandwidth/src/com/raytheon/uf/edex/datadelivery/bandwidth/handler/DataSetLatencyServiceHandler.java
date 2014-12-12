e6849e7f543bbb29b4ca6d59411482cf1fe5b2ef
Chris Cody <Chris.Cody@raytheon.com>
2014-12-12 15:57:39 -0600

    Omaha #3550a Fix Build (third)
    Change-Id: I848298a9fa356d4eb19e1bafc69df0cd28c47dce

--- edex/com.raytheon.uf.edex.datadelivery.bandwidth/src/com/raytheon/uf/edex/datadelivery/bandwidth/handler/DataSetLatencyServiceHandler.java
+++ edex/com.raytheon.uf.edex.datadelivery.bandwidth/src/com/raytheon/uf/edex/datadelivery/bandwidth/handler/DataSetLatencyServiceHandler.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.uf.edex.datadelivery.service.services;
+package com.raytheon.uf.edex.datadelivery.bandwidth.handler;
 
 import com.raytheon.uf.common.auth.exception.AuthorizationException;
 import com.raytheon.uf.common.auth.user.IUser;
