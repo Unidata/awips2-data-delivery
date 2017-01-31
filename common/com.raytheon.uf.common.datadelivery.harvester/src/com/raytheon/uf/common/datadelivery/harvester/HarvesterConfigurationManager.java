06b40011ec19c650504791ecb6740dc6a9507953
Tom Gurney <tom.gurney@raytheon.com>
2017-01-31 09:25:05 -0600

    Omaha #5684 Log connection URLs on DPA startup
    Change-Id: If79641eb544e17bf0201bd397fa3b1c0845e76ae

--- common/com.raytheon.uf.common.datadelivery.harvester/src/com/raytheon/uf/common/datadelivery/harvester/HarvesterConfigurationManager.java
+++ common/com.raytheon.uf.common.datadelivery.harvester/src/com/raytheon/uf/common/datadelivery/harvester/HarvesterConfigurationManager.java
@@ -3,19 +3,19 @@ package com.raytheon.uf.common.datadelivery.harvester;
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -28,8 +28,8 @@ import javax.xml.bind.JAXBException;
 
 import com.raytheon.uf.common.datadelivery.registry.Connection;
 import com.raytheon.uf.common.datadelivery.registry.Provider;
-import com.raytheon.uf.common.datadelivery.registry.ProviderType;
 import com.raytheon.uf.common.datadelivery.registry.Provider.ServiceType;
+import com.raytheon.uf.common.datadelivery.registry.ProviderType;
 import com.raytheon.uf.common.localization.IPathManager;
 import com.raytheon.uf.common.localization.LocalizationFile;
 import com.raytheon.uf.common.localization.PathManagerFactory;
@@ -41,9 +41,9 @@ import com.raytheon.uf.common.status.UFStatus.Priority;
 
 /**
  * Manages harvester configurations
- * 
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
@@ -51,27 +51,28 @@ import com.raytheon.uf.common.status.UFStatus.Priority;
  * Oct 23, 2013 2361       njensen     Use JAXBManager for XML
  * Oct 28, 2013 2361       dhladky     Fixed up JAXBManager.
  * Jun 14, 2014 3120       dhladky     PDA
- * 
+ * Jan 31, 2017 5684       tgurney     Add connection URL log message
+ *
  * </pre>
- * 
+ *
  * @author dhladky
- * @version 1.0
  */
 public class HarvesterConfigurationManager {
 
     private static final IUFStatusHandler statusHandler = UFStatus
             .getHandler(HarvesterConfigurationManager.class);
-    
+
     private static final Class<?>[] clazzess = new Class<?>[] {
-        HarvesterConfig.class, Provider.class, Connection.class,
-        ProviderType.class, ServiceType.class, Agent.class,
-        CrawlAgent.class, OGCAgent.class, PDAAgent.class, ConfigLayer.class };
+            HarvesterConfig.class, Provider.class, Connection.class,
+            ProviderType.class, ServiceType.class, Agent.class,
+            CrawlAgent.class, OGCAgent.class, PDAAgent.class,
+            ConfigLayer.class };
 
     private static JAXBManager jaxb = null;
-    
+
     /**
      * marshall and unmarshall harvester objects
-     * 
+     *
      * @return
      */
     private static JAXBManager getJaxb() {
@@ -90,7 +91,7 @@ public class HarvesterConfigurationManager {
 
     /**
      * Gets site and base level configs for harvesters
-     * 
+     *
      * @return
      */
     public static List<LocalizationFile> getLocalizedFiles() {
@@ -105,7 +106,7 @@ public class HarvesterConfigurationManager {
 
     /**
      * Get the crawler configuration
-     * 
+     *
      * @return
      */
     public static HarvesterConfig getCrawlerConfiguration() {
@@ -130,7 +131,7 @@ public class HarvesterConfigurationManager {
 
     /**
      * Get the OGC configuration
-     * 
+     *
      * @return
      */
     public static HarvesterConfig getOGCConfiguration() {
@@ -154,10 +155,10 @@ public class HarvesterConfigurationManager {
 
         return config;
     }
-    
+
     /**
      * Get the PDA configuration
-     * 
+     *
      * @return
      */
     public static HarvesterConfig getPDAConfiguration() {
@@ -184,7 +185,7 @@ public class HarvesterConfigurationManager {
 
     /**
      * Get this harvester configuration File
-     * 
+     *
      * @param file
      * @return
      */
@@ -193,8 +194,17 @@ public class HarvesterConfigurationManager {
         HarvesterConfig config = null;
 
         try {
-            config = getJaxb()
-                    .unmarshalFromXmlFile(HarvesterConfig.class, file);
+            config = getJaxb().unmarshalFromXmlFile(HarvesterConfig.class,
+                    file);
+            if (config.getProvider() != null
+                    && config.getProvider().getConnection() != null
+                    && config.getProvider().getConnection().getUrl() != null) {
+
+                statusHandler.info("Got connection URL for provider "
+                        + config.getProvider().getName() + ": "
+                        + config.getProvider().getConnection().getUrl());
+            }
+
         } catch (SerializationException e) {
             statusHandler.handle(Priority.ERROR,
                     "Can't deserialize harvester config at " + file.getPath(),
@@ -207,7 +217,7 @@ public class HarvesterConfigurationManager {
 
     /**
      * Writes the harvester config files
-     * 
+     *
      * @param config
      * @param file
      */
