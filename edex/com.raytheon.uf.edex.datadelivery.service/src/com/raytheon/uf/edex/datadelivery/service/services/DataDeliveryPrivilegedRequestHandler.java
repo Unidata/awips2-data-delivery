9c5bd8686dec9e2011a54a89275da72c18303cda
Ron Anderson <ron.anderson@raytheon.com>
2017-07-19 13:26:12 -0500

    Omaha #6286 Migrate Data Delivery to new roles/permissions framework
    Change-Id: I639ba170811551783f230b7653ef2cc3d0ab48da

--- edex/com.raytheon.uf.edex.datadelivery.service/src/com/raytheon/uf/edex/datadelivery/service/services/DataDeliveryPrivilegedRequestHandler.java
+++ edex/com.raytheon.uf.edex.datadelivery.service/src/com/raytheon/uf/edex/datadelivery/service/services/DataDeliveryPrivilegedRequestHandler.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -21,60 +21,49 @@ package com.raytheon.uf.edex.datadelivery.service.services;
 
 import com.raytheon.uf.common.auth.exception.AuthorizationException;
 import com.raytheon.uf.common.auth.req.AbstractPrivilegedRequest;
-import com.raytheon.uf.common.auth.user.IUser;
+import com.raytheon.uf.common.datadelivery.request.DataDeliveryAuthRequest;
 import com.raytheon.uf.common.datadelivery.request.DataDeliveryPermission;
-import com.raytheon.uf.common.plugin.nwsauth.NwsAuthRequest;
-import com.raytheon.uf.edex.auth.AuthManager;
 import com.raytheon.uf.edex.auth.AuthManagerFactory;
-import com.raytheon.uf.edex.auth.authorization.IAuthorizer;
+import com.raytheon.uf.edex.auth.IPermissionsManager;
 import com.raytheon.uf.edex.auth.req.AbstractPrivilegedRequestHandler;
 import com.raytheon.uf.edex.auth.resp.AuthorizationResponse;
 
 /**
  * Handler for Data Delivery Privileged Requests.
- * 
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
- * 
- * Date         Ticket#    Engineer    Description
- * ------------ ---------- ----------- --------------------------
- * Apr 12, 2012  224       mpduff      Initial creation
- * Oct 03, 2012  1241      djohnson    Use {@link DataDeliveryPermission}.
- * Jul 26, 2013  2232      mpduff      Refactored Data Delivery permissions.
- * May 29, 2014  3211      njensen     Use IAuthorizer instead of IRoleStorage
- * 
+ *
+ * Date          Ticket#  Engineer  Description
+ * ------------- -------- --------- --------------------------------------------
+ * Apr 12, 2012  224      mpduff    Initial creation
+ * Oct 03, 2012  1241     djohnson  Use {@link DataDeliveryPermission}.
+ * Jul 26, 2013  2232     mpduff    Refactored Data Delivery permissions.
+ * May 29, 2014  3211     njensen   Use IAuthorizer instead of IRoleStorage
+ * Jul 18, 2017  6286     randerso  Changed to use new Roles/Permissions
+ *                                  framework
+ *
  * </pre>
- * 
+ *
  * @author mpduff
- * @version 1.0
  */
 
 public class DataDeliveryPrivilegedRequestHandler<T extends AbstractPrivilegedRequest>
         extends AbstractPrivilegedRequestHandler<T> {
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.raytheon.uf.edex.auth.req.AbstractPrivilegedRequestHandler#authorized
-     * (com.raytheon.uf.common.auth.user.IUser,
-     * com.raytheon.uf.common.auth.req.AbstractPrivilegedRequest)
-     */
     @Override
-    public AuthorizationResponse authorized(IUser user, T request)
+    public AuthorizationResponse authorized(T request)
             throws AuthorizationException {
-        if (request instanceof NwsAuthRequest) {
-            NwsAuthRequest authRequest = (NwsAuthRequest) request;
+        if (request instanceof DataDeliveryAuthRequest) {
+            DataDeliveryAuthRequest authRequest = (DataDeliveryAuthRequest) request;
 
-            AuthManager manager = AuthManagerFactory.getInstance().getManager();
-            IAuthorizer auth = manager.getAuthorizer();
+            IPermissionsManager manager = AuthManagerFactory.getInstance()
+                    .getPermissionsManager();
 
             boolean addedAuthorization = false;
-
             for (String permission : authRequest.getRequestedPermissions()) {
-                boolean authorized = auth.isAuthorized(permission.toString(),
-                        user.uniqueId().toString(), "Data Delivery");
+                boolean authorized = manager.isPermitted(permission);
                 addedAuthorization |= authorized;
 
                 if (authorized) {
@@ -93,13 +82,6 @@ public class DataDeliveryPrivilegedRequestHandler<T extends AbstractPrivilegedRe
         return new AuthorizationResponse(false);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.raytheon.uf.common.serialization.comm.IRequestHandler#handleRequest
-     * (com.raytheon.uf.common.serialization.comm.IServerRequest)
-     */
     @Override
     public Object handleRequest(T request) throws Exception {
         /*
@@ -107,8 +89,8 @@ public class DataDeliveryPrivilegedRequestHandler<T extends AbstractPrivilegedRe
          * do here. If we get here then we are authorized, set true in request
          * object and return it
          */
-        if (request instanceof NwsAuthRequest) {
-            NwsAuthRequest r = (NwsAuthRequest) request;
+        if (request instanceof DataDeliveryAuthRequest) {
+            DataDeliveryAuthRequest r = (DataDeliveryAuthRequest) request;
             r.setAuthorized(true);
         }
 
