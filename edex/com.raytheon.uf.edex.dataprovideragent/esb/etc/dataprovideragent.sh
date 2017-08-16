a0f9b584a38f73b1d954a78e7c06345eb917d769
Nathan Bowler <Nathan.Bowler@raytheon.com>
2017-08-16 13:47:49 -0400

    Omaha #6366 Pluginize edex shell scripts
    Change-Id: I3877682bd304cbc9c17d74b12a369c27fc9d7abd

---
+++ edex/com.raytheon.uf.edex.dataprovideragent/esb/etc/dataprovideragent.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+##
+# This software was developed and / or modified by Raytheon Company,
+# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+# 
+# U.S. EXPORT CONTROLLED TECHNICAL DATA
+# This software product contains export-restricted data whose
+# export/transfer/disclosure is restricted by U.S. law. Dissemination
+# to non-U.S. persons whether in the United States or abroad requires
+# an export license or other authorization.
+# 
+# Contractor Name:        Raytheon Company
+# Contractor Address:     6825 Pine Street, Suite 340
+#                         Mail Stop B8
+#                         Omaha, NE 68106
+#                         402.291.0100
+# 
+# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+# further licensing information.
+##
+
+##
+# The data provider agent mode is designed to take a data feed from a provider,
+# store it locally, send the correct product information to a registry, and
+# then share the data out via WCS/WMS requests.
+##
+
+export MAX_MEM=1536 # in Meg
+export EDEX_DEBUG_PORT=5010
+
