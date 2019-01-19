3733ee5d08465a4bb7fc83d3f40dc7a4e482bb54
mjames-upc <mjames@unidata.ucar.edu>
2019-01-18 21:12:41 -0700

    rm madis

--- edex/deploy.edex-Data_Delivery/esb/bin/centralRegistryProviderCredentials.sh
+++ edex/deploy.edex-Data_Delivery/esb/bin/centralRegistryProviderCredentials.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-##
-# This software was developed and / or modified by Raytheon Company,
-# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
-# 
-# U.S. EXPORT CONTROLLED TECHNICAL DATA
-# This software product contains export-restricted data whose
-# export/transfer/disclosure is restricted by U.S. law. Dissemination
-# to non-U.S. persons whether in the United States or abroad requires
-# an export license or other authorization.
-# 
-# Contractor Name:        Raytheon Company
-# Contractor Address:     6825 Pine Street, Suite 340
-#                         Mail Stop B8
-#                         Omaha, NE 68106
-#                         402.291.0100
-# 
-# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
-# further licensing information.
-##
 SETUP_ENV=/awips2/edex/bin/setup.env
 source $SETUP_ENV
 JAVA_BIN=/awips2/java/jre/bin/java
